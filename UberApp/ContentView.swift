35036cd49b002389840d0f3f8bb904f77815ea4b
Kevin Hernandez <kmhs-18@hotmail.com>
2023-05-19 12:42:17 -0300

    Initial Commit

---
+++ UberApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UberApp
+//
+//  Created by Kevin Hernandez on 19/05/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ UberApp/UberAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberAppApp.swift
+//  UberApp
+//
+//  Created by Kevin Hernandez on 19/05/23.
+//
+
+import SwiftUI
+
+@main
+struct UberAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
